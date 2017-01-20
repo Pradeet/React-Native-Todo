7bb8b2284be39ff067fb13574ddb552f6ddbe6bc
Pradeet Swamy <pradeet1995@gmail.com>
2017-01-20 19:54:21 +0530

    Fixed adding empty Todo fixed. Todo: Integrate Firebase, Added delete, edit, complete Todo Features.

--- application/Components/TodoInput.js
+++ application/Components/TodoInput.js
@@ -14,6 +14,9 @@ export default class TodoInput extends Component {
 
     onSubmit() {
         let text = this.state.text;
+        if (text === '') {
+            return
+        }
         this.setState({
             text: '',
         }, () => {
